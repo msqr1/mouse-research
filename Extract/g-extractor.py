6be9cfa10ed79eda4bbd3f09d7783db15da7b4ba
msqr1 <134968689+msqr1@users.noreply.github.com>
2025-05-04 14:48:53 -0700

    Fix extractors

---
+++ Extract/g-extractor.py
@@ -0,0 +1,64 @@
+import sys, glob, xlsxwriter
+
+weekDir = sys.argv[1]
+output = sys.argv[2]
+animalDirs = glob.glob(f"{weekDir}/*")
+animalIDs = [i[len(weekDir) + 1:] for i in animalDirs]
+book = xlsxwriter.Workbook(output)
+
+apex = book.add_worksheet("Apex")
+pp = book.add_worksheet("PP")
+smv = book.add_worksheet("SMV")
+longAxis = book.add_worksheet("Long Axis")
+for i in [apex, pp, smv]:
+  i.write_row("A1", ["ID", "MyoGCS", "EndoGCS", "EndoROT", "MyoROT"])
+
+longAxis.write_row("A1", ["ID", "MyoGCS*", "MyoGLS", "EndoGCS*", "EndoGLS"])
+
+# Information line number:
+
+# apex, pp and smv:
+#   101: MyoGCS
+#   102: EndoGCS
+#   106: EndoRot
+#   107: MyoROT
+
+# long axis:
+#   105: MyoGCS*
+#   106: MyoGLS
+#   107: EndoGCS*
+#   108: EndoGLS
+
+arr = [
+  [apex, "Apex*/*Analysis(ref. view apex).txt"],
+  [pp, "PP*/*Analysis(ref. view pm).txt"],
+  [smv, "sMV*/*Analysis(ref. view mv).txt"]
+]
+
+for i in range(len(animalDirs)):
+  for j in arr:
+    j[0].write(1 + i, 0, animalIDs[i])
+    a = glob.glob(f"{animalDirs[i]}/{j[1]}")
+    if(len(a)) > 0:
+      with open(a[0]) as f:
+        for k in range(100):
+          next(f)
+        myoGCS = float(f.readline()[7:-3])
+        endoGCS = float(f.readline()[8:-3])
+        for k in range(3):
+          next(f)
+        endoROT = float(f.readline()[8:-5])
+        myoROT = float(f.readline()[7:-5])
+        j[0].write_row(1 + i, 1, [myoGCS, endoGCS, endoROT, myoROT])
+  longAxis.write(1 + i, 0, animalIDs[i])
+  a = glob.glob(f"{animalDirs[i]}/Long Axis*/*Analysis(ref. view pslax).txt")
+  if(len(a)) > 0:
+    with open(a[0]) as f:
+      for k in range(0, 104):
+        next(f)
+      myoGCS = float(f.readline()[8:-3])
+      myoGLS = float(f.readline()[7:-3])
+      endoGCS = float(f.readline()[9:-3])
+      endoGLS = float(f.readline()[8:-3])
+      longAxis.write_row(1 + i, 1, [myoGCS, myoGLS, endoGCS, endoGLS])
+book.close()
--- Extract/strain-extractor.py
+++ Extract/strain-extractor.py
@@ -11,22 +11,11 @@ def getStrainIterables(globExpr, cellCnt):
   IDs = []
   for i in range(animalCnt):
     a = glob.glob(f"{animalDirs[i]}/{globExpr}.xml")
-    if len(a) > 0:
-      XMLs.append(a[0])
-      IDs.append(animalIDs[i])
+    IDs.append(animalIDs[i])
+    XMLs.append(a[0] if len(a) > 0 else '')
   return zip(range(2, len(IDs) * cellCnt, cellCnt), XMLs, IDs, strict=True)
 
 book = xlsxwriter.Workbook(output)
-apex = book.add_worksheet("Apex")
-pp = book.add_worksheet("PP")
-smv = book.add_worksheet("SMV")
-
-for i in [apex, pp, smv]:
-  i.write_row("A1", ["ID", "Cell type"])
-  i.merge_range("C1:E1", "Endo")
-  i.merge_range("F1:H1", "Myo")
-  i.merge_range("I1:K1", "Epi")
-  i.write_row("C2", ["TTP", "Peak", "ES"] * 3)
 
 class Input():
   def __init__(self, sheet, cellTp, globExpr):
@@ -35,14 +24,31 @@ class Input():
     self.cellCnt = len(cellTp)
     self.globExpr = globExpr
 
+apex = book.add_worksheet("Apex")
+pp = book.add_worksheet("PP")
+smv = book.add_worksheet("SMV")
+longAxis = book.add_worksheet("Long Axis")
+
+for i in [apex, pp, smv, longAxis]:
+  i.write_row("A1", ["ID", "Cell type"])
+  i.merge_range("C1:E1", "Endo")
+  i.merge_range("F1:H1", "Myo")
+  i.merge_range("I1:K1", "Epi")
+  i.write_row("C2", ["TTP", "Peak", "ES"] * 3)
+
 for input in [
   Input(apex, ["13-apical anterior", "16-apical lateral", "15-apical inferior", "14-apical septal"], "Apex*/*TTP(apex)"),
   Input(pp, ["07-mid anterior", "12-mid anterolateral", "11-mid inferolateral", "10-mid inferior", "09-mid inferoseptal", "08-mid anteroseptal"], "PP*/*TTP(pm)"),
-  Input(smv, ["01-basal anterior", "06-basal anterolateral", "05-basal inferolateral", "04-basal inferior", "03-basal inferoseptal", "02-basal anteroseptal"], "sMV*/*TTP(mv)")
+  Input(smv, ["01-basal anterior", "06-basal anterolateral", "05-basal inferolateral", "04-basal inferior", "03-basal inferoseptal", "02-basal anteroseptal"], "sMV*/*TTP(mv)"),
+  Input(longAxis, ["05-basal inferolateral", "11-mid inferolateral", "16-apical lateral", "17-apex", "14-apical septal", "08-mid anteroseptal", "02-basal anteroseptal"], "Long Axis*/*TTP(pslax)")
 ]:
   maxRowLen = 12 + input.cellCnt * 2
   skipRadial = 10 + input.cellCnt
   for i, xml, id  in getStrainIterables(input.globExpr, input.cellCnt):
+    input.sheet.merge_range(i, 0, i + input.cellCnt - 1, 0, id)
+    input.sheet.write_column(i, 1, input.cellTp)
+    if xml == '':
+      continue
     worksheets = None
     with open(xml) as f:
       worksheets = xmltodict.parse(f.read())["Workbook"]["Worksheet"]
@@ -59,12 +65,9 @@ for input in [
           data[j].append(cells[1]["Data"]["#text"])
           data[j].append(cells[2]["Data"]["#text"])
           data[j].append(cells[3]["Data"]["#text"])
-    input.sheet.merge_range(i, 0, i + input.cellCnt - 1, 0, id)
-    input.sheet.write_column(i, 1, input.cellTp)
     for j in range(input.cellCnt):
       input.sheet.write_row(i + j, 2, data[j])
-
-for i in [apex, pp, smv]:
+for i in [apex, pp, smv, longAxis]:
   i.autofit()
 
 book.close()
