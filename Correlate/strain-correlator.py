db3a042e2e46015c48547ac070707e0b40843899
msqr1 <134968689+msqr1@users.noreply.github.com>
2025-05-05 02:00:35 -0700

    2am and i'm dying

--- Correlate/strain-correlator.py
+++ Correlate/strain-correlator.py
@@ -23,7 +23,7 @@ def getMVData(input, top, left):
 dataFiles = glob.glob(f"{sys.argv[1]}/*.xlsx")
 dataFiles.sort()
 wbs = [openpyxl.load_workbook(i, read_only=True) for i in dataFiles]
-book = xlsxwriter.Workbook(sys.argv[3])
+book = xlsxwriter.Workbook(sys.argv[4])
 apex = book.add_worksheet("Apex")
 apexs = [i["Apex"] for i in wbs]
 pp = book.add_worksheet("PP")
@@ -32,12 +32,13 @@ smv = book.add_worksheet("SMV")
 smvs = [i["SMV"] for i in wbs]
 longAxis = book.add_worksheet("Long Axis")
 longAxes = [i["Long Axis"] for i in wbs]
+
 mvData = []
 with open(sys.argv[2]) as f:
   reader = csv.reader(f)
   for i in reader:
     mvData.append(i)
-for i in zip([apex, pp, smv, longAxis], [apexs, pps, smvs, longAxes], [4, 6, 6, 7]):
+for i in zip([apex, pp], [apexs, pps], [4, 6]):
   i[0].write_row("A3", ["ID", "", *["Coefficient", "P-value"] * 9])
   for j,k in zip(range(2, 15, 6), ["Endo", "Myo", "Epi"], strict=True):
     i[0].merge_range(0, j, 0, j + 5, k)
@@ -48,17 +49,42 @@ for i in zip([apex, pp, smv, longAxis], [apexs, pps, smvs, longAxes], [4, 6, 6, 
   # k is for strain
   for j,k in zip(range(1, 22, 4), range(3, 4 + 5 * i[2], i[2]), strict=True):
     funcData = [getMVData(mvData, l, 4) for l in range(j, j + 4)]
-    print(funcData)
-    print()
     strainData = [getStrainData(i[1], k, l, i[2]) for l in range(3, 12)]
-    print(strainData)
-    for l in funcData:
+    for l in enumerate(funcData):
       data = []
       for m in strainData:
-        res = scipy.stats.pearsonr(l, m)
+        res = scipy.stats.pearsonr(l[1], m)
         data.append(res.correlation)
         data.append(res.pvalue)
-      i[0].write_row(j + 2, 2, data)
+      i[0].write_row(j + 2 + l[0], 2, data)
+    i[0].merge_range(j + 2, 0, j + 5, 0, mvData[j][0])
+    i[0].write_column(j + 2, 1, ["AET", "IVCT", "IVRT", "MV E"])
+  i[0].autofit()
+
+mvData = []
+with open(sys.argv[3]) as f:
+  reader = csv.reader(f)
+  for i in reader:
+    mvData.append(i)
+for i in zip([smv, longAxis], [smvs, longAxes], [6, 7]):
+  i[0].write_row("A3", ["ID", "", *["Coefficient", "P-value"] * 9])
+  for j,k in zip(range(2, 15, 6), ["Endo", "Myo", "Epi"], strict=True):
+    i[0].merge_range(0, j, 0, j + 5, k)
+    for l,m in zip(range(j, j + 6, 2), ["TTP", "Peak", "Es"], strict=True):
+      i[0].merge_range(1, l, 1, l + 1, m)
+
+  # j is for mv and output sheet
+  # k is for strain
+  for j,k in zip(range(1, 22, 4), range(3, 4 + 5 * i[2], i[2]), strict=True):
+    funcData = [getMVData(mvData, l, 4) for l in range(j, j + 4)]
+    strainData = [getStrainData(i[1], k, l, i[2]) for l in range(3, 12)]
+    for l in enumerate(funcData):
+      data = []
+      for m in strainData:
+        res = scipy.stats.pearsonr(l[1], m)
+        data.append(res.correlation)
+        data.append(res.pvalue)
+      i[0].write_row(j + 2 + l[0], 2, data)
     i[0].merge_range(j + 2, 0, j + 5, 0, mvData[j][0])
     i[0].write_column(j + 2, 1, ["AET", "IVCT", "IVRT", "MV E"])
   i[0].autofit()
